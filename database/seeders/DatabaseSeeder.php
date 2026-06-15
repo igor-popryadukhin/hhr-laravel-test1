5c394e12761e770c6b3c8fd260af013cb53f0dcb
popryadukhin.i <igorpopryadukhin@gmail.com>
2026-06-15 14:12:17 +0300

    Use firstOrCreate in seeder to avoid duplicate key errors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,10 +20,12 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'),
-        ]);
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => bcrypt('password'),
+            ],
+        );
     }
 }
